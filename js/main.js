84b42810f1736b51af4607feb85e3206f5e761a7
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-06-13 13:02:56 +0700

    исправление ошибок lint

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-  let comment = ''; //Строка с комментарием
-  const LENGTH_COMMENT = 140;
+let comment = ''; //Строка с комментарием
+const LENGTH_COMMENT = 140;
   
-  function returnRandomInt (min, max) {
-    if (min > 0 && max > 0) {
-      if (min > max) {         //меняет местами min и max если  min > max
-        let variable = min;
-        min = max;
-        max = variable;
-      } 
-      let rand = min + Math.random() * (max + 1 - min);
-      return Math.floor(rand);
+function returnRandomInt (min, max) {
+  if (min > 0 && max > 0) {
+    if (min > max) {         //меняет местами min и max если  min > max
+      let variable = min;
+      min = max;
+      max = variable;
     }
-    console.log ('Диапазон введен неверно'); //если диапазон с отрицательным числом
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
   }
+  console.log ('Диапазон введен неверно'); //если диапазон с отрицательным числом
+}
   
-  returnRandomInt (5, 7);
+returnRandomInt (5, 7);
   
-  function checkCommentLenght (string, length) {    //сравнивает строку с максимальным колличеством символов
-    return string.length <= length ? true : false;
-  }
+function checkCommentLenght (string, length) {    //сравнивает строку с максимальным колличеством символов
+  return string.length <= length ? true : false;
+}
   
-  checkCommentLenght (comment, LENGTH_COMMENT);
+checkCommentLenght (comment, LENGTH_COMMENT);
