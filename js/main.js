bc9fe6064f0b5cdd5d6d2e730642d280638fdf68
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-06-13 12:48:16 +0700

    добавление функции проверки максимальной длинны

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
-function returnRandomInt (min, max) {
-  if (min > 0 && max > 0) {
-    if (min > max) {
-      let variable = min;
-      min = max;
-      max = variable;
-    } 
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
+  let comment = ''; //Строка с комментарием
+  const LENGTH_COMMENT = 140;
+  
+  function returnRandomInt (min, max) {
+    if (min > 0 && max > 0) {
+      if (min > max) {         //меняет местами min и max если  min > max
+        let variable = min;
+        min = max;
+        max = variable;
+      } 
+      let rand = min + Math.random() * (max + 1 - min);
+      return Math.floor(rand);
+    }
+    console.log ('Диапазон введен неверно'); //если диапазон с отрицательным числом
   }
-  console.log ('Диапазон введен неверно');
-}
-
-returnRandomInt (5, 7);
+  
+  returnRandomInt (5, 7);
+  
+  function checkCommentLenght (string, length) {    //сравнивает строку с максимальным колличеством символов
+    return string.length <= length ? true : false;
+  }
+  
+  checkCommentLenght (comment, LENGTH_COMMENT);
