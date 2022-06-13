3541a84f8bc9e9b3e76d175defabd6ec1ed9af7b
Dima <dima_sokolov92@bk.ru>
2022-06-13 14:08:26 +0700

    Исправление по Lint

--- js/main.js
+++ js/main.js
@@ -1,23 +1,27 @@
-let comment = ''; //Строка с комментарием
+const comment = ''; //Строка с комментарием
 const LENGTH_COMMENT = 140;
-  
+
 function returnRandomInt (min, max) {
   if (min > 0 && max > 0) {
     if (min > max) {         //меняет местами min и max если  min > max
-      let variable = min;
+      const variable = min;
       min = max;
       max = variable;
     }
-    let rand = min + Math.random() * (max + 1 - min);
+    const rand = min + Math.random() * (max + 1 - min);
     return Math.floor(rand);
   }
+  // eslint-disable-next-line no-console
   console.log ('Диапазон введен неверно'); //если диапазон с отрицательным числом
 }
-  
+
 returnRandomInt (5, 7);
-  
+
 function checkCommentLenght (string, length) {    //сравнивает строку с максимальным колличеством символов
-  return string.length <= length ? true : false;
+  if (string.length <= length) {
+    return true;
+  }
+  return false;
 }
-  
+
 checkCommentLenght (comment, LENGTH_COMMENT);
