c64af60698a41c6cba81064a8640e3d6a94d8b7b
Dima <dima_sokolov92@bk.ru>
2022-06-21 13:29:14 +0700

    Добавление доработанной фун-ии рандомного числа

--- js/main.js
+++ js/main.js
@@ -1,20 +1,14 @@
 const comment = ''; //Строка с комментарием
 const LENGTH_COMMENT = 140;
 
-const returnRandomInt = (min, max) => {
-  if (min > 0 && max > 0) {
-    if (min > max) {         //меняет местами min и max если  min > max
-      const variable = min;
-      min = max;
-      max = variable;
-    }
-    const rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-  }
-  return false;
+const getRandomPositiveInteger = (a, b) => {     //Возвращает рандомное число в положительном диапазоне
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
 };
 
 const checkCommentLenght = (string, length) => string.length <= length;    //сравнивает строку с максимальным колличеством символов
 
-returnRandomInt (11, 5);
+getRandomPositiveInteger (100, 5);
 checkCommentLenght (comment, LENGTH_COMMENT);
