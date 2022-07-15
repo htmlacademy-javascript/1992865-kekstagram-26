cd4cf8a29631ab6be31f0e4a8cfcb28d4a7bd1e1
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-07-15 15:29:59 +0700

    Доработка функции добавления рандомных фотографий

--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -2,17 +2,17 @@ import {photos} from './data-creation.js';
 
 const pictures = document.querySelector ('.pictures');
 const pictureTemplate = document.querySelector('#picture').content;
-const similarListFragment = document.createDocumentFragment();
 
 const addingPhoto = (photographies) => {
+  const similarListFragment = document.createDocumentFragment();
   photographies.forEach(({url, likes, comments}) => {
     const photographiesElement = pictureTemplate.cloneNode(true);
     photographiesElement.querySelector('.picture__img').src = url;
     photographiesElement.querySelector('.picture__likes').textContent = likes;
     photographiesElement.querySelector('.picture__comments').textContent = comments.length;
     similarListFragment.append(photographiesElement);
+    pictures.append(similarListFragment);
   });
 };
 
 addingPhoto(photos);
-pictures.append(similarListFragment);
