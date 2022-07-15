981096d73fe1359fb5815a5a8517138c99c3877e
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-07-15 14:40:13 +0700

    Добавление фотографий других пользователей

--- js/thumbnail-rendering.js
+++ js/thumbnail-rendering.js
@@ -4,14 +4,14 @@ const pictures = document.querySelector ('.pictures');
 const pictureTemplate = document.querySelector('#picture').content;
 const similarListFragment = document.createDocumentFragment();
 
-const addingPhoto = (photos) => {
-  photos.forEach(({url, likes, comments}) => {
-    const photoElement = pictureTemplate.cloneNode(true);
-    photoElement.querySelector('.picture__img').src = url;
-    photoElement.querySelector('.picture__likes').textContent = likes;
-    photoElement.querySelector('.picture__comments').textContent = comments.length;
-  
-    similarListFragment.append(photoElement);
+const addingPhoto = (photographies) => {
+  photographies.forEach(({url, likes, comments}) => {
+    const photographiesElement = pictureTemplate.cloneNode(true);
+    photographiesElement.querySelector('.picture__img').src = url;
+    photographiesElement.querySelector('.picture__likes').textContent = likes;
+    photographiesElement.querySelector('.picture__comments').textContent = comments.length;
+    
+    similarListFragment.append(photographiesElement);
   });
 };
 
