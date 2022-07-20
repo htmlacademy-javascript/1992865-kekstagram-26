148dd8178315562573381b5d05686caee78ad03d
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-07-21 00:44:54 +0700

    Заполнение данными фотографии в раскрытом виде

--- js/window-big-picture.js
+++ js/window-big-picture.js
@@ -1,31 +1,41 @@
 /* 
 1. Заведите модуль, который будет отвечать за отрисовку окна с полноразмерным изображением. ++
 
-2. Для отображения окна нужно удалять класс hidden у элемента .big - picture и каждый раз заполнять его данными о конкретной фотографии:
+2. Для отображения окна нужно удалять класс hidden у элемента .big-picture и каждый раз заполнять его данными о конкретной фотографии:
 
-  - Адрес изображения url подставьте как src изображения внутри блока.big - picture__img.
+  - Адрес изображения url подставьте как src изображения внутри блока .big-picture__img. ++
 
-  - Количество лайков likes подставьте как текстовое содержание элемента.likes - count.
+  - Количество лайков likes подставьте как текстовое содержание элемента .likes-count. ++
 
-  - Количество комментариев comments подставьте как текстовое содержание элемента.comments - count.
+  - Количество комментариев comments подставьте как текстовое содержание элемента .comments-count. ++
 
-  - Список комментариев под фотографией: комментарии должны вставляться в блок.social__comments.
+  - Список комментариев под фотографией: комментарии должны вставляться в блок .social__comments.
   
-  - Описание фотографии description вставьте строкой в блок.social__caption.
+  - Описание фотографии description вставьте строкой в блок .social__caption. ++
   
-3. После открытия окна спрячьте блоки счётчика комментариев.social__comment-count и загрузки новых комментариев.comments-loader, добавив им класс hidden, с ними мы разберёмся позже, в другом домашнем задании.
+3. После открытия окна спрячьте блоки счётчика комментариев .social__comment-count и загрузки новых комментариев .comments-loader, добавив им класс hidden, с ними мы разберёмся позже, в другом домашнем задании.
   
-4. После открытия окна добавьте тегу < body > класс modal - open, чтобы контейнер с фотографиями позади не прокручивался при скролле.При закрытии окна не забудьте удалить этот класс. ++
+4. После открытия окна добавьте тегу <body> класс .modal-open, чтобы контейнер с фотографиями позади не прокручивался при скролле.При закрытии окна не забудьте удалить этот класс. ++
   
 5. Напишите код для закрытия окна по нажатию клавиши Esc и клике по иконке закрытия.++
   
 6. Подключите модуль в проект.++
 */
+import {photos} from './data-creation.js';
+console.log(photos);
 
-const picture = document.querySelectorAll ('.picture');
+const pictures = document.querySelectorAll ('.picture');
 const body = document.querySelector ('body');
-const bigPicture = document.querySelector ('.big-picture');
-const buttonBigPictureCancel = document.querySelector ('.big-picture__cancel');
+const bigPictureElement = document.querySelector ('.big-picture');
+const buttonBigPictureCancel = bigPictureElement.querySelector ('.big-picture__cancel');
+
+const addingPhotoComments = () => {
+  bigPictureElement.querySelectorAll('.social__comments');
+  console.log();
+  photos.comments.forEach(({avatar, name, message}) => {
+    
+  });
+};
 
 const onBigPictureEscKeydown = (evt) => {
   if (evt.key === 'Escape') {
@@ -34,22 +44,28 @@ const onBigPictureEscKeydown = (evt) => {
   }
 };
 
-const openBigPicture = (pictures) => {
-  for (var i = 0; i < pictures.length; i++) {
-    pictures[i].addEventListener ('click', () => {
-      bigPicture.classList.remove('hidden');
-      body.classList.add ('modal-open');
+const openBigPicture = (pictures, photos) => {
+  pictures.forEach ((element, index) => {
+    element.addEventListener('click', () => {
+      bigPictureElement.classList.remove('hidden');
+      body.classList.add('modal-open');
+      bigPictureElement.querySelector ('.big-picture__img').querySelector('img').src = photos[index].url;
+      bigPictureElement.querySelector ('.likes-count').textContent = photos[index].likes;
+      bigPictureElement.querySelector ('.comments-count').textContent = photos[index].comments.length;
+      bigPictureElement.querySelector ('.social__caption').textContent = photos[index].description;
+      bigPictureElement.querySelector('.social__comment-count').classList.add('hidden');
+      bigPictureElement.querySelector('.comments-loader').classList.add('hidden');
     });
-  };
+  });
   
   document.addEventListener('keydown', onBigPictureEscKeydown);
 };
 
 const closeBigPicture = () => {
-  bigPicture.classList.add('hidden');
+  bigPictureElement.classList.add('hidden');
   body.classList.remove('modal-open');
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
+  bigPictureElement.querySelector('.social__comment-count').classList.remove('hidden');
+  bigPictureElement.querySelector('.comments-loader').classList.remove('hidden');
   
   document.removeEventListener('keydown', onBigPictureEscKeydown);
 };
@@ -58,4 +74,4 @@ buttonBigPictureCancel.addEventListener ('click', () => {
   closeBigPicture();
 });
 
-openBigPicture(picture);
+openBigPicture(pictures, photos);
