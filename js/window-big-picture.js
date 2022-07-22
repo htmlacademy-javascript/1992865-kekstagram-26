b24b5be0756e469cb1bf7281fc2cd026c56108a9
dima_sokolov92@bk.ru <dima_sokolov92@bk.ru>
2022-07-22 15:57:15 +0700

    Исправление недочетов

--- js/window-big-picture.js
+++ js/window-big-picture.js
@@ -1,6 +1,7 @@
 import {photos} from './data-creation.js';
+
 const pictures = document.querySelectorAll ('.picture');
-const body = document.querySelector ('body');
+const body = document.body;
 const bigPictureElement = document.querySelector ('.big-picture');
 const buttonBigPictureCancel = bigPictureElement.querySelector ('.big-picture__cancel');
 
@@ -11,19 +12,19 @@ const addingPhotoComments = (index) => {
   const similarListFragment = document.createDocumentFragment();
   photos[index].comments.forEach(({avatar, name, message}) => {
     const photosCommentsClone = socialComment.cloneNode(true);
-    photosCommentsClone.querySelector('.social__picture').src = avatar;
-    photosCommentsClone.querySelector('.social__picture').alt = name;
+    const socialPictureElementClone = photosCommentsClone.querySelector('.social__picture');
+    socialPictureElementClone.src = avatar;
+    socialPictureElementClone.alt = name;
     photosCommentsClone.querySelector('.social__text').textContent = message;
     similarListFragment.append(photosCommentsClone);
     socialComments.append(similarListFragment);
   });
 };
 
-const onBigPictureEscKeydown = (evt) => {
+const onBigPictureEscKeydown = function (evt) {
   if (evt.key === 'Escape') {
     evt.preventDefault();
-    // eslint-disable-next-line no-use-before-define
-    closeBigPicture(); //не понял почему linter ругается на это, т.к. это ни где в критериях не запрещенно
+    closeBigPicture();
   }
 };
 
@@ -32,7 +33,7 @@ const openBigPicture = (picture, photogphiess) => {
     element.addEventListener('click', () => {
       bigPictureElement.classList.remove('hidden');
       body.classList.add('modal-open');
-      bigPictureElement.querySelector('.big-picture__img').querySelector('img').src = photogphiess[index].url;
+      bigPictureElement.querySelector('.big-picture__img img').src = photogphiess[index].url;
       bigPictureElement.querySelector('.likes-count').textContent = photogphiess[index].likes;
       bigPictureElement.querySelector('.comments-count').textContent = photogphiess[index].comments.length;
       bigPictureElement.querySelector('.social__caption').textContent = photogphiess[index].description;
